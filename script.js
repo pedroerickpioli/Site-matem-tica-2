287c4ab216e964f84bf7f0ae36dc67e6cf2fb112
erickpioli <110614961+pedroerickpioli@users.noreply.github.com>
2024-06-20 08:17:43 -0300

    projeto 2°trimestre

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const perguntas = [
         enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
+                texto: "Isso é assustasdor!",
                 afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
             },
             {
